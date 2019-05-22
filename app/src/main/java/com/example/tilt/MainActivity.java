14b7728e3266d404852d77db4ab6f88a96bcaf89
Ville <dat15vak@student.lu.se>
2019-05-22 15:36:22 +0200

    Added tutourial popup

--- app/src/main/java/com/example/tilt/MainActivity.java
+++ app/src/main/java/com/example/tilt/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.tilt;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Dialog myDialog;
     // TODO: Package Management
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDialog = new Dialog(this);
     }
 
     public void playOnClick(View v){
@@ -22,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
     public void highScoreOnClick(View v){
         Intent i = new Intent(this, HighScoreActivity.class);
         startActivity(i);
+    }
+
+    public void ShowPopup(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("M");
+        btnFollow = (Button) myDialog.findViewById(R.id.btnext);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
